e92ba9748dceff9b53ef5fe5f3c7d0e26f7f05aa
Jonathan Mooney <moonejon@gmail.com>
2024-05-21 09:11:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# ChartHop, React Technical Screener
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Actions
+
+In the project directory, you can run:
+
+### `npm install`
+
+Installs all project dependencies.
+
+### `npm start`
+
+Runs the app in development mode. \
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits. \
+You will also see any lint errors in the console.
+
+## The Challenge
+
+To complete this challenge, you will:
+- Copy the files to a new repository on your personal GitHub account
+- Solve the technical problem described below
+- Explain your solution to the problem
+- Share your repository with the ChartHop team
+
+### The Problem
+
+As it stands, the header renders itself on every keystroke to the search input. This is indicated by the `Header` component as a message, "Header rendered {n} times". In this exercise, you are asked to modify the app so that the header does not re-render when a new keystroke is entered into the search input. This task must be accomplished without changing the signatures of any functions or functional components.
+
+### The Solution
+
+Once you've arrived at a solution, please add a paragraph to this README, describing the nature of the problem you fixed (why did the app render on every keystroke?), the solution you provided, and an explanation as to why your solution corrects the problem. When you're ready, please share your repository with the ChartHop team. Specific sharing accounts will be provided via email.
---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import { Box, FormControlLabel, Input, InputLabel, Radio, RadioGroup, Stack } from '@mui/material';
+import { createContext, useContext, useEffect, useState } from 'react';
+
+enum IceCreamShop {
+    WHATS_THE_SCOOP = "What's the Scoop",
+    CONE_KINGDOM = "Cone Kingdom",
+    WENDYS = "Wendy's",
+}
+
+const IceCreamContext = createContext<{ flavors: string[], filter: string, updateFilter: (filter: string) => void }>({ } as any);
+
+const Filter = () => {
+  const { filter, updateFilter } = useContext(IceCreamContext);
+  return (
+    <div>
+        <InputLabel>Search for your favorite flavor</InputLabel>
+        <Input value={filter} onChange={(event) => updateFilter(event.target.value)} />
+    </div>
+  );
+};
+
+const VisibleFlavors = ({fetchFlavors}: {fetchFlavors: () => string[]}) => {
+  const { filter } = useContext(IceCreamContext);
+  const flavors = fetchFlavors();
+  const visibleFlavors = flavors.filter((flavor) => flavor.includes(filter));
+
+  return (
+    <div>
+        {visibleFlavors.map((flavor) => <div key={flavor}>{flavor}</div>)}
+    </div>
+  );
+}
+
+const Header = ({ fetchFlavors }: { fetchFlavors: () => string[] }) => {
+  const [renderCount, setRenderCount] = useState(0);
+  const flavors = fetchFlavors();
+  useEffect(() => setRenderCount(renderCount => renderCount + 1), [fetchFlavors]);
+
+  return (
+    <div>
+        <h1>Choose from our {flavors.length} options</h1>
+        <p>Header rendered {renderCount} times</p>
+    </div>
+  );
+}
+
+export const App = () => {
+  const [iceCreamShop, setIceCreamShop] = useState(IceCreamShop.WHATS_THE_SCOOP);
+  const [filter, setFilter] = useState("");
+  const updateFilter = (filter: string) => {
+    setFilter(filter);
+  }
+
+  const fetchFlavors = (): string[] => {
+    switch (iceCreamShop) {
+        case IceCreamShop.WHATS_THE_SCOOP:
+            return ["vanilla", "chocolate", "strawberry", "mint chip", "rocky road", "birthday cake", "cookie dough", "coffee", "pistachio", "butter pecan", "cotton candy", "bubble gum", "peanut butter", "black cherry"];
+        case IceCreamShop.CONE_KINGDOM:
+            return ["ube", "matcha oreo", "corn bread", "fruit loops", "s'mores", "black sesame", "lemon", "funnel cake"]
+        case IceCreamShop.WENDYS:
+            return ["chocolate frosty", "vanilla frosty", "strawberry frosty"]
+    }
+  };
+
+  return (
+    <Box pl={3} pt={3}>
+        <InputLabel>Viewing ice cream from</InputLabel>
+        <RadioGroup row name="ice-cream-shop" value={iceCreamShop} onChange={(_e, shop) => setIceCreamShop(shop as IceCreamShop)}>
+            <FormControlLabel value={IceCreamShop.WHATS_THE_SCOOP} control={<Radio/>} label={IceCreamShop.WHATS_THE_SCOOP} />
+            <FormControlLabel value={IceCreamShop.CONE_KINGDOM} control={<Radio/>} label={IceCreamShop.CONE_KINGDOM} />
+            <FormControlLabel value={IceCreamShop.WENDYS} control={<Radio/>} label={IceCreamShop.WENDYS} />
+        </RadioGroup>
+        <Stack spacing={3} direction="column">
+        <Header fetchFlavors={fetchFlavors} />
+        <IceCreamContext.Provider value={{ flavors: [], filter, updateFilter}}>
+            <Filter />
+            <VisibleFlavors fetchFlavors={fetchFlavors}/>
+        </IceCreamContext.Provider>
+        </Stack>
+    </Box>
+  );
+}
